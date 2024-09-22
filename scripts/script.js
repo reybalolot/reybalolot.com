1989a97a9a55beefaf8b952a62035f27fe79fbfc
balolot.johnrey <balolot.johnrey@gmail.com>
2024-09-22 20:35:26 +0800

    added cursor

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const cursor = document.querySelector('.cursor');
+const card = document.querySelectorAll('.card');
+document.addEventListener('mousemove', (e) => {
+    cursor.classList.remove('cursor-hide');
+    cursor.setAttribute('style',`left: ${e.pageX + 7}px; top: ${e.pageY - 5}px`)
+})
+
+card.forEach(el => {
+    el.addEventListener('mouseenter', () => {
+        cursor.classList.add('cursor-green');
+    })
+    el.addEventListener('mouseleave', () => {
+        cursor.classList.remove('cursor-green');
+    })
+})
