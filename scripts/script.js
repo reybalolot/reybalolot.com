53122f071573d3d329a3e2b7aa8802584e3a6c2e
balolot.johnrey <balolot.johnrey@gmail.com>
2024-11-29 06:58:49 +0800

    added notetip

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,12 @@
 const cursor = document.querySelector('.cursor');
+const cursorTip = document.querySelector('.cursor-note');
 const card = document.querySelectorAll('.card');
+const projectName = document.querySelectorAll('.name');
+
 document.addEventListener('mousemove', (e) => {
-    cursor.classList.remove('cursor-hide');
+    // cursor.classList.remove('cursor-hide');
     cursor.setAttribute('style',`left: ${e.pageX + 7}px; top: ${e.pageY - 5}px`)
+    cursorTip.setAttribute('style',`left: ${e.pageX + 15}px; top: ${e.pageY - 5}px`)
 })
 
 card.forEach(el => {
@@ -11,5 +15,16 @@ card.forEach(el => {
     })
     el.addEventListener('mouseleave', () => {
         cursor.classList.remove('cursor-green');
+    })
+})
+
+projectName.forEach(el => {
+    el.addEventListener('mouseenter', () => {
+        cursor.classList.add('cursor-hidden');
+        cursorTip.classList.remove('cursor-hidden');
+    })
+    el.addEventListener('mouseleave', () => {
+        cursor.classList.remove('cursor-hidden');
+        cursorTip.classList.add('cursor-hidden');
     })
 })
